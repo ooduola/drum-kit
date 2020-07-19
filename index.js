18bc43f71bf3cd7d2e2a0a5bb7c2d4e2af8b57ab
Olatunji Oduola <ola.oduola@gmail.com>
2020-07-19 22:34:04 +0100

    Added background image using css to drum squares

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i< numberOfDrumButtons; i++) {
 
   document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-    alert("clicked yay!");
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+
   });
 
 }
